78de2036184094ab6e79e23bad82e4b3e5bff633
Stuart Halloway <stu@thinkrelevance.com>
2008-06-04 18:49:18 -0400

    added xss check to tarantula test

--- rails_samples/test/tarantula/tarantula_test.rb
+++ rails_samples/test/tarantula/tarantula_test.rb
@@ -3,10 +3,19 @@ require "relevance/tarantula"
 
 class TarantulaTest < ActionController::IntegrationTest
   fixtures :all
-
+  include Relevance::Tarantula
+  
   def test_tarantula
     post '/users/login', :login => 'quentin', :password => 'test'
     follow_redirect!
-    tarantula_crawl(self)
+    attack = { :name => 'script', 
+      :input => '<script>Banzai!</script>',
+      :output => '<script>Banzai!</script>'}
+    XssFormSubmission.attacks << attack
+    t = tarantula_crawler(self)
+    t.handlers << XssDocumentCheckerHandler.new
+    t.fuzzers << XssFormSubmission
+    t.times_to_crawl = 2
+    t.crawl "/"
   end
 end
