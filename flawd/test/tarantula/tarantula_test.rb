f89efa2b1736369d4d3b0ad00c84624ce54c6166
Stuart Halloway <stu@thinkrelevance.com>
2008-06-04 21:43:00 -0400

    working tarantula test

---
+++ flawd/test/tarantula/tarantula_test.rb
@@ -0,0 +1,10 @@
+require "#{File.dirname(__FILE__)}/../test_helper"
+require "relevance/tarantula"
+
+class TarantulaTest < ActionController::IntegrationTest
+  fixtures :all
+
+  def test_tarantula
+    tarantula_crawl(self, :url => 'posts')
+  end
+end
