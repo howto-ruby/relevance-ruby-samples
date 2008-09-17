898288711b43332e6c2d963e7b0ab1dd21a3084f
Jason Rudolph <github@jasonrudolph.com>
2008-09-17 10:45:34 -0400

    renaming and a correction ...
      - Remove incorrect (and failing) call to line.split in part 3 of processing_data solution
      - Rename "steps" to "parts" (to match the codecite naming convention)

--- ruby_samples/structs/part3/read_people.rb
+++ ruby_samples/structs/part3/read_people.rb
@@ -4,7 +4,6 @@ require 'csv'
 Person = Struct.new(*CSV.parse_line(gets).map{|l| l.downcase.to_sym})
 # codecite inject
 people = readlines.inject([]) do |people,line|
-  line.split!
   people << Person.new(*CSV.parse_line(line))
 end
 # codecite inject
