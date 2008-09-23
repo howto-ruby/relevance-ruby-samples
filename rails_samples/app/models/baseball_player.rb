cc9926b4832f7a93a0fd92f13bec2d67dcb52cc6
Jason Rudolph <github@jasonrudolph.com>
2008-09-23 15:39:48 -0400

    Add basic demonstration on named scopes

--- rails_samples/app/models/baseball_player.rb
+++ rails_samples/app/models/baseball_player.rb
@@ -1,2 +1,4 @@
 class BaseballPlayer < ActiveRecord::Base
+  named_scope :slugger, :conditions => ['batting_average > ?', 0.400]
+  named_scope :rookie, lambda { { :conditions => ['created_at > ?', 1.year.ago] } }
 end
